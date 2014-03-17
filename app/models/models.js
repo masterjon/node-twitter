7e728e578b67fbb616af061c013d89636ebf5e18
masterjon <jonabc7@gmail.com>
2014-03-17 13:48:30 -0600

    added mongolab support

--- app/models/models.js
+++ app/models/models.js
@@ -1,5 +1,19 @@
 var mongoose=require('mongoose');
 
-mongoose.connect('mongodb://localhost/'+'backendPro')
+// Heroku: Here we find an appropriate database to connect to, defaulting to
+// localhost if we don't find one.
+var uristring = 
+  process.env.MONGOLAB_URI || 
+  process.env.MONGOHQ_URL || 
+  'mongodb://localhost/'+'backendPro';
+
+mongoose.connect(uristring, function (err, res){
+    if (err) { 
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + uristring);
+  }
+    
+})
 
 module.exports=mongoose
